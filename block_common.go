2957d3628562611beb29923dd04d224c7127b696
Derrick Petzold <derrick@petzold.io>
2018-05-15 23:25:30 -0700

    const

--- block_common.go
+++ block_common.go
@@ -6,15 +6,17 @@
 
 package termui
 
-const TOP_RIGHT = '┐'
-const VERTICAL_LINE = '│'
-const HORIZONTAL_LINE = '─'
-const TOP_LEFT = '┌'
-const BOTTOM_RIGHT = '┘'
-const BOTTOM_LEFT = '└'
-const VERTICAL_LEFT = '┤'
-const VERTICAL_RIGHT = '├'
-const HORIZONTAL_DOWN = '┬'
-const HORIZONTAL_UP = '┴'
-const QUOTA_LEFT = '«'
-const QUOTA_RIGHT = '»'
+const (
+	TOP_RIGHT       = '┐'
+	VERTICAL_LINE   = '│'
+	HORIZONTAL_LINE = '─'
+	TOP_LEFT        = '┌'
+	BOTTOM_RIGHT    = '┘'
+	BOTTOM_LEFT     = '└'
+	VERTICAL_LEFT   = '┤'
+	VERTICAL_RIGHT  = '├'
+	HORIZONTAL_DOWN = '┬'
+	HORIZONTAL_UP   = '┴'
+	QUOTA_LEFT      = '«'
+	QUOTA_RIGHT     = '»'
+)
